bf74e956edb156edf1be75f4fde64ff67dc959d8
JoseFelix-R <83871334+JoseFelix-R@users.noreply.github.com>
2022-02-02 11:06:40 -0300

    Update

--- projeto_final_modelo.py
+++ projeto_final_modelo.py
@@ -1,14 +1,12 @@
 import pandas as pd
 
-
 class Video:
     '''
     Representa um vídeo do Youtube.
     '''
-
     def __init__(self, idvideo='padrao', titulo='padrao', dt_publicacao='01/01/2022', idcanal='idpadrao', canal='padrao', datat='0/0/0', cont_views=0, likes=0, dislikes=0, cont_comentarios=0, descricao='padrao', categoria='ALL'):
         self.idvideo = idvideo
-        self.titulo = titulo
+        self.titulo = titulo 
         self.datap = dt_publicacao
         self.idcanal = idcanal
         self.canal = canal
@@ -19,11 +17,28 @@ class Video:
         self.qcom = cont_comentarios
         self.desc = descricao
         self.catin = categoria
-
+        
+    def inicializador(self, video):
+        '''Inicializa as informações do Video'''
+        self.idvideo = video[0]
+        self.titulo = video[1] 
+        self.datap = video[2] 
+        self.idcanal = video[3] 
+        self.canal = video[4] 
+        self.datat = video[5] 
+        self.qvisu = video[6] 
+        self.qlikes = video[7] 
+        self.qdislikes = video[8] 
+        self.qcom = video[9]
+        self.desc = video[10]
+        self.catin = video[11]
+   
+        
     def __str__(self):
-        '''Metodo usado para saida das Informações de um video do youtube'''
-        return f'{self.titulo} - {self.datap} - {self.canal} - {self.datat} - {self.qvisu} - {self.qlikes} - {self.qdislikes} - {self.qcom} - {self.desc} - {self.catin} '
-
+        '''Metodo usado para saida das Informações de um video do youtube'''    
+        return f'{self.canal}\n - {self.titulo}\n - {self.catin}\n - Views: {str(self.qvisu)}\n - Comentarios: {self.qcom}\n - Likes: {self.qlikes}\n Publicado em: {self.datap}\n {"------------------------------"}'
+    
+        
 
 class BaseDeDados:
     '''
@@ -31,63 +46,118 @@ class BaseDeDados:
     responsável por realizar consultas
     em um arquivo Pandas.Dataframe.
     '''
-
+    
     def __init__(self, nome_arq):
         '''
         Inicializa uma base de dados
         com o nome do arquivo (.csv)
         '''
-        # abre o dataframe e o atribui a um atributo de instância
+        
+        ## abre o dataframe e o atribui a um atributo de instância
         self.df = pd.read_csv(str(nome_arq), lineterminator='\n')
-        # VEJA SEÇÃO 2.3.3 altera tipo das colunas data
-
-        # SUBSTITUA df ABAIXO pelo atributo de instância
-        # correspondente ao dataframe
+        
+        self.c = BaseDeDados.convert(self) 
+        
+        ## VEJA SEÇÃO 2.3.3 altera tipo das colunas data
+        self.df.dt_publicacao = pd.to_datetime(self.df.dt_publicacao)
+        self.df.dt_trending = pd.to_datetime(self.df.dt_trending)
+        
+        ## SUBSTITUA df ABAIXO pelo atributo de instância
+        ## correspondente ao dataframe
         print(f'Arquivo: {nome_arq}')
         print(f'Possui dados dos vídeos em tendência no Youtube BR')
         print(f'Total de vídeos: {len(self.df)}')
-        print(
-            f'Período: {self.df.dt_publicacao.min()} até {self.df.dt_publicacao.max()}')
+        print(f'Período: {self.df.dt_publicacao.min()} até {self.df.dt_publicacao.max()}')
         print(f'Dados dos vídeos:')
         for c in self.df.columns.to_list():
             print(c, end=', ')
-
+    
     def lista_categorias(self):
         '''
         Retorna lista contendo todas as categorias
         presentes no dataframe.
         '''
-
-        # SUBSTITUA df ABAIXO pelo atributo de instância
-        # correspondente ao dataframe
+        
+        ## SUBSTITUA df ABAIXO pelo atributo de instância
+        ## correspondente ao dataframe
         return list(self.df.categoria.unique())
+    
+    def convert(self):
+        res = [tup for tup in zip(self.df.id_video, self.df.titulo,
+                          self.df.dt_publicacao, self.df.id_canal,
+                          self.df.canal, self.df.dt_trending,
+                          self.df.cont_views, self.df.likes,
+                          self.df.dislikes, self.df.cont_comentarios,
+                          self.df.descricao, self.df.categoria)]
+        
+        i = Video.inicializador(self, res)
+      
+    def busca_por_titulo(self, titulo):
+        print('Buscando por titulo')
+        r = []  
+        c = self.df.titulo.str.contains(titulo, case=False)
+        print(c)
+        for i in c:
+            if c == True:
+                r.append(tup)
+        return r
+        
+            
 
+    def busca_por_canal(self, nome_canal):
+        res = self.df[self.df.canal == nome_canal]
+        display(res)
+    
+    def busca_por_categoria(self, categoria):
+        categ = {'29': 'Nonprofits & Activism',
+         '1': 'Film & Animation', 
+         '2': 'Autos & Vehicles', 
+         '10': 'Music', 
+         '15': 'Pets & Animals', 
+         '17': 'Sports', 
+         '18': 'Short Movies', 
+         '19': 'Travel & Events', 
+         '20': 'Gaming', 
+         '21': 'Videoblogging', 
+         '22': 'People & Blogs', 
+         '23': 'Comedy', 
+         '24': 'Entertainment', 
+         '25': 'News & Politics', 
+         '26': 'Howto & Style', 
+         '27': 'Education', 
+         '28': 'Science & Technology', 
+         '30': 'Movies', 
+         '31': 'Anime/Animation', 
+         '32': 'Action/Adventure', 
+         '33': 'Classics', 
+         '34': 'Comedy', 
+         '35': 'Documentary', 
+         '36': 'Drama', 
+         '37': 'Family', 
+         '38': 'Foreign', 
+         '39': 'Horror', 
+         '40': 'Sci-Fi/Fantasy', 
+         '41': 'Thriller', 
+         '42': 'Shorts', 
+         '43': 'Shows', 
+         '44': 'Trailers'}
+       
+        res = self.df[self.df.categoria == categ['24']]
+        display(res)
+    
+    def busca_por_periodo(self,inicio, fim):
+        self.df.dt_publicacao = pd.to_datetime(self.df.dt_publicacao)
+        self.df.dt_trending = pd.to_datetime(self.df.dt_trending)
+        # Retorna um dataframe contendo todos os vídeos
+        # no período que envolve a data 'ini' e data 'fim'
+        masc = (self.df.dt_publicacao.dt.date >= pd.to_datetime(inicio)) & (self.df.dt_publicacao.dt.date <= pd.to_datetime(fim))
 
-if __name__ == '__main__':
-    ld = BaseDeDados('BR_youtube_trending_data_completo.csv')
-    print('\n\n\n')
-    print(ld.lista_categorias())
-    #res = ld.busca_por_titulo('fla')
-    #res = ld.busca_por_canal('espor')
-    # res = ld.busca_por_categoria('SPORTS') # também pode ser informado um nr. inteiro
-    #res = ld.busca_por_periodo('2020-11-01', '2020-11-30')
+        # masc é uma expressão que gera um 'vetor' de True/False; este vetor é então utilizado para retornar o dataframe resultante
+        res = self.df[masc]
+        display(res)
 
-    # print('\n\n\n')
-    # for v in res:
-    #  print(v)
-    print('\n\n\n')
-    v = Video()
-    v.idvideo = 'YT@#$34'
-    v.titulo = 'Tiringa Rindo'
-    v.datap = '24/01/2021'
-    v.idcanal = 'BR$#$%SEL'
-    v.canal = 'Brasil Selvagem'
-    v.datat = '30/11/2021'
-    v.qvisu = 10900089
-    v.qlikes = 120098
-    v.qdislikes = 1190
-    v.qcom = 845
-    v.desc = 'Esse é um video do tiringa se divertindo com Tõe'
-    v.catin = 'Comedy'
+    def __str__(self):
+        s = '\nSaida do Programa:\n' 
+        
+        return s
 
-    print(v)
