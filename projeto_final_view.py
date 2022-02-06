9ab57e0c74091f750c7a65544e72410bf182b5c5
JoseFelix-R <83871334+JoseFelix-R@users.noreply.github.com>
2022-02-05 21:37:18 -0300

    Atualizado sábado

--- projeto_final_view.py
+++ projeto_final_view.py
@@ -9,11 +9,14 @@ class BuscaGUI:
         
         self.botoes= {}
         self._tv = None
+        self._r = '0'
+        self._ltv = []
         self._root = root
         self._categoria = ['Comedia', 'Entretenimento', 'Esportes', 'Documentarios', 'Educacional','Games','Musica']        
+        
         self._inicializar_vars()
         self._inicializa_gui()
-        
+   
     def _inicializar_vars(self):
         self._titulo = tk.StringVar()
         self._canal = tk.StringVar()
@@ -21,41 +24,44 @@ class BuscaGUI:
         self._data_f = tk.StringVar()     
         self._categ = tk.StringVar()
         self.nome_arq = tk.StringVar()
+        self._quant = tk.StringVar()
     
-    def seleciona_arquivo():
-        tipos_arq = (
-            ('Arquivos de texto', '*.txt'),
-            ('Arquivos CSV', '*.csv'),
-            ('Todos os arquivos', '*.*')
-        )
-
-        nome_arq  = askopenfilename(title='Abrir arquivo',\
-                                   filetypes=tipos_arq)
-        if nome_arq:
-            nome_arq.set('Arquivo escolhido: ' + nome_arq)
+    def _add_dados(self):
+        '''Add os dados de entrada do usuario'''
+        return(self._titulo.get(), self._canal.get(), self._data_i.get(), self._data_f, self._categ)
     
     def _inicializa_gui(self):
-        ttk.Style().theme_use('vista')
-        frame_top = tk.Frame(self._root, bd=10, relief=tk.SUNKEN)
-        frame_top.config(bg='red')
+        #Tema TTK
+        ttk.Style().theme_use('default')
+
+        #Frames Interface
+        frame_top = ttk.Frame(self._root)
         frame_top.pack(expand=True, fill=tk.BOTH, side=tk.TOP)
-        frame_right = tk.Frame(self._root, bd=10, relief=tk.SUNKEN)
+        frame_right = ttk.Frame(self._root)
         frame_right.pack(expand=True, fill=tk.BOTH, side=tk.RIGHT) 
-        frame_down = tk.Frame(self._root, bd = 10, relief=tk.SUNKEN)
+        frame_down = ttk.Frame(self._root)
         frame_down.pack(expand=True, fill=tk.BOTH, side=tk.BOTTOM) 
         
-        colunas = ['0','1','2','3','4','5','6']
+        #TreeView 
+        colunas = ['0','1','2','3','4','5','6','7','8','9','10','11']
         self._tv = ttk.Treeview(frame_right, columns=colunas, show='headings')
-        self._tv.pack(expand=True, fill=tk.BOTH, side=tk.TOP) 
+        self._tv.pack(expand=True, fill=tk.BOTH, side=tk.TOP)
+        
         
         #heading
-        self._tv.heading('0', text='Canal')
+        self._tv.heading('0', text='Id_video')
         self._tv.heading('1', text='Titulo')
-        self._tv.heading('2', text='Categoria')
-        self._tv.heading('3', text='Views')
-        self._tv.heading('4', text='Comentatios')
-        self._tv.heading('5', text='Likes')
-        self._tv.heading('6', text='Publicacao')
+        self._tv.heading('2', text='Publicacao')
+        self._tv.heading('3', text='Id_canal')
+        self._tv.heading('4', text='Canal')
+        self._tv.heading('5', text='Threding')
+        self._tv.heading('6', text='Visualizações')
+        self._tv.heading('7', text='Likes')
+        self._tv.heading('8', text='Dislikes')
+        self._tv.heading('9', text='Comentarios')
+        self._tv.heading('10', text='Descrução')
+        self._tv.heading('11', text='Categoria')
+        
         
         self._tv.column('0', width=90, minwidth=100)
         self._tv.column('1', width=90, minwidth=100)
@@ -64,6 +70,11 @@ class BuscaGUI:
         self._tv.column('4', width=90, minwidth=100)
         self._tv.column('5', width=90, minwidth=100)
         self._tv.column('6', width=90, minwidth=100)
+        self._tv.column('7', width=90, minwidth=100)
+        self._tv.column('8', width=90, minwidth=100)
+        self._tv.column('9', width=90, minwidth=100)
+        self._tv.column('10', width=90, minwidth=100)
+        self._tv.column('11', width=90, minwidth=100)
         
         #ScrollBar    
         sb_y = ttk.Scrollbar(frame_right, orient=tk.VERTICAL, command=self._tv.yview)
@@ -78,30 +89,30 @@ class BuscaGUI:
         
         
         #Menu de Busca
-        v_titu = tk.Label(frame_top, text='Titulo: ')
+        v_titu = ttk.Label(frame_top, text='Titulo: ')
         v_titu.grid(row = 0, column = 0)
         e_titu = tk.Entry(frame_top, width=40,textvariable=self._titulo)
         e_titu.grid(row = 0, column = 1, columnspan=3, sticky='W')
     
         
-        v_canal = tk.Label(frame_top, text='Canal: ')
+        v_canal = ttk.Label(frame_top, text='Canal: ')
         v_canal.grid(row = 1, column = 0)
         e_canal = tk.Entry(frame_top, width=20, textvariable=self._canal)
         e_canal.grid(row = 1, column = 1, sticky='W')
         
-        v_data_i = tk.Label(frame_top, text='Inicio: ')
+        v_data_i = ttk.Label(frame_top, text='Inicio: ')
         v_data_i.grid(row = 2, column = 0)
         e_data_i= tk.Entry(frame_top, width=10, textvariable=self._data_i)
         e_data_i.grid(row = 2, column = 1, sticky='W')
         
-        v_data_f = tk.Label(frame_top, text='Final: ')
+        v_data_f = ttk.Label(frame_top, text='Final: ')
         v_data_f.grid(row = 2, column = 2)
         e_data_f= tk.Entry(frame_top, width=10, textvariable=self._data_f)
-        e_data_f.grid(row = 2, column = 3, sticky='W')
+        e_data_f.grid(row = 2, column = 3, sticky='E')
         
         #Combobox categoria
         
-        v_cat = tk.Label(frame_top, text='Categoria: ')
+        v_cat = ttk.Label(frame_top, text='Categoria: ')
         v_cat.grid(row = 3, column = 0, sticky='S')
         v_cb = ttk.Combobox(frame_top, width = 20, textvariable=self._categ,
                            state='readonly', values=self._categoria)
@@ -109,28 +120,37 @@ class BuscaGUI:
         
 
         #Butões
-        self.botoes['Buscar'] = tk.Button(frame_top, width=10, text='Buscar')
+        self.botoes['Buscar'] = ttk.Button(frame_top,text='Buscar')
         self.botoes['Buscar'].grid(row = 4, column = 3, sticky='E')
-        self.botoes['limpar'] = tk.Button(frame_top, width=10, text='limpar')
-        self.botoes['limpar'].grid(row = 4, column = 4, sticky='E')
+        self.botoes['Limpar'] = ttk.Button(frame_top,text='Limpar')
+        self.botoes['Limpar'].grid(row = 4, column = 4, sticky='E')
         
         self.nome_arq.set('Arquivo escolhido: ')
-        self.botoes['A_Arquivo'] = tk.Button(frame_top, width=10, text='Abrir Arquivo', command=BuscaGUI.seleciona_arquivo)
+        self.botoes['A_Arquivo'] = ttk.Button(frame_top, text='Abrir Arquivo')
         self.botoes['A_Arquivo'].grid(row = 4, column = 5, sticky='E')
         
+        #Display quantidade de Videos
         
+        self._quant.set(self._r)
+        v_quant = ttk.Label(frame_top,text='Videos encontrados: ')
+        v_quant.grid(row = 7, column=0, columnspan=3, sticky='S')
+        v_quant = ttk.Label(frame_top, text='0')
+        v_quant.grid(row = 7, column=2, sticky='S')
+        
+    def _atualiza_tv(self, lista):
+        '''Modifica os valores da TreeVeiw'''
+     
+        for i,vi in enumerate(lista):
+            self._ltv.append(vi)
+
+        for i in self._ltv:
+                self._tv.insert('', tk.END, values=i)
+
 if __name__ == '__main__':
     root = tk.Tk()
     root.title('Buscador Youtube')
-
     gui = BuscaGUI(root)
-    #gui.atualiza_listbox(['um', 'dois', 'tres']) # Observe: LISTA DE STRINGS
+    gui._atualiza_tv([['um','dois', 'tres','quatro','cinco','seis','sete','oito','nove','dez','onze','doze'],
+    ['um','dois', 'tres','quatro','cinco','seis','sete','oito','nove','dez','onze','doze']])
 
     root.mainloop()
-        
-
-        
-        
-        
-        
-        
